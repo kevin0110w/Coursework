6a80ff5937c02755b09ad33bb9c4e1ae385e2db0
kevin0110w <37250646+kevin0110w@users.noreply.github.com>
2018-09-18 17:41:37 +0100

    Create Exercise2.1:.java

---
+++ Programming  COMPSCI4039/Exercise2.1:.java
@@ -0,0 +1,10 @@
+
+public class Lab2Ex1 {
+	public static void main(String[] args) {
+		String message = "Hello World!";
+		System.out.println(message);
+		
+		message = "Goodbye!";
+		System.out.println(message);
+	}
+}
