9c3473e0045a509a8b0877a65471dab54375bd70
kevin0110w <37250646+kevin0110w@users.noreply.github.com>
2018-09-22 22:21:48 +0100

    Update Exercise6i:firstTenTerms.java

--- Programming  COMPSCI4039/Exercise6i:firstTenTerms.java
+++ Programming  COMPSCI4039/Exercise6i:firstTenTerms.java
@@ -19,3 +19,26 @@ public class Lab6Ex1 {
 	}
 
 }
+
+//  or with a do loop.
+
+import java.util.Scanner;
+
+public class TimesTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please type a number: ");
+		int number = Integer.parseInt(s.nextLine());
+		int i = 1;
+		do {
+			int sum = number * i;
+			System.out.println(number + " x " + i + " = " + sum);
+			i++;
+		} while (i <= 10);
+		s.close();
+	}
+
+}
