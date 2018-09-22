3be60c1dbec4beadba62e323fa350830da71e3bf
kevin0110w <37250646+kevin0110w@users.noreply.github.com>
2018-09-22 22:11:37 +0100

    Update and rename Exercise4.1 to Exercise4i:isLeapYear.java

--- Programming  COMPSCI4039/Exercise4i:isLeapYear.java
+++ Programming  COMPSCI4039/Exercise4i:isLeapYear.java
@@ -1,4 +1,6 @@
-
+/*
+Implement your code for computing if a particular year is a leap year. Test your code with a suitable set of test cases.
+*/
 public class Lab4Ex1 {
 	public static void main(String[] args) {
 		int year = 2356;
