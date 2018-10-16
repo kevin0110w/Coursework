10d4c84105a920ef59eefd2957039f8aab777c6b
kevin0110w <37250646+kevin0110w@users.noreply.github.com>
2018-10-16 17:18:47 +0100

    Create Person.java

---
+++ Programming  COMPSCI4039/Week-2/Exercise03/BankAccountWithException/Person.java
@@ -0,0 +1,43 @@
+
+public class Person {
+	private String name;
+	private int age;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public int setAge(int age) {
+		return this.age = age;
+	}
+	
+	public String setName(String name) {
+		return this.name = name;
+	}
+	
+	public void sayHello() {
+		System.out.println("Hi, my name is " + this.name + ". My age is " + this.age + ".");
+	}
+	
+	public String toString() {
+		return "I am " + this.name + " and my age is " + this.age + ".";
+	}
+	
+	public String sayWhat() {
+		return this.name + " says WHAT!";
+	}
+
+}
