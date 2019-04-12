4c90879e85d9afa02baf7fce3e92c5423f72c5f8
Kevin Wu <0808148w@student.gla.ac.uk>
2019-04-12 20:28:29 +0100

    working on some code from ads sample paper

---
+++ ADS/ADSStuff.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ADSStuff {
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    list.add("A");
+    list.add("B");
+    list.add("C");
+    list.add("D");
+    for (int i = 0; i < list.size(); i++) {
+      System.out.print(list.remove(i));
+    }
+  }
+}
---
+++ ADS/ADSStuff2.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+/*
+ * Some code from the ADS Sample Paper
+ */
+public class ADSStuff2 {
+
+	public static void main(String[] args) {
+//	    List<String> list = new ArrayList<>();
+//	    list.add("A");
+//	    list.add("B");
+//	    list.add("C");
+//	    list.add("D");
+//	    for (int i = 0; i < list.size(); i++) {
+//	      System.out.print(list.remove(i));
+//	    }
+		
+		
+//		Set<A> set = new HashSet<A>();
+//		set.add(new A());
+//		set.add(new A());
+//		set.add(new A());
+//		set.add(new A());
+//		System.out.println(set);
+//	}
+//	
+//	public static class A {
+//		int r = 1;
+//		public String toString() {
+//			return r + "";
+//		}
+//		
+//		public int hashCode() {
+//			return r;
+//		}
+//		Map<String, String> map = new HashMap<String, String>();
+//		map.put("123", "John Smith");
+//		map.put("111", "Ella Smith");
+//		map.put("123", "Steve Price");
+//		map.put("222", "Steve Price");
+//		System.out.println(map);
+		
+//	    ArrayList<Integer> list = new ArrayList<>();
+//	    list.add(1);
+//	    list.add(2);
+//	    list.add(3);
+//	    list.add(4);
+//	    list.remove(2);
+//	    System.out.println(list);
+		
+//		HashSet<String> set1 = new HashSet<String>();
+//		set1.add("bubblegum");
+//		set1.add("starburst");
+//		HashSet<String> set2 = (HashSet<String>) (set1.clone());
+//		System.out.println(set1);
+//		System.out.println(set2);
+//		set1.add("smarties");
+//		set2.add("dolly mixture");
+//		set1.remove("bubblegum");
+//		set1.add("smarties");
+//		System.out.println(set2);
+//		System.out.println(set1);
+		
+		Map<Integer, String> map = new HashMap<Integer, String>(); 
+		 map.put(100, "Darth Vader"); 
+		 map.put(75, "Kylo Ren"); 
+		 map.put(150, "Yoda"); 
+		 map.put(200, "Han Solo"); 
+		 map.put(150, "R2-D2"); 
+		 map.put(150, "Boba Fett"); 
+		 System.out.println(map);
+	}
+}
+
